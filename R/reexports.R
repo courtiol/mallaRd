965ccc928fc50c3d3833315a61798e78a21c0511
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2024-01-11 15:38:51 +0100

    Format data and check spelling

---
+++ R/reexports.R
@@ -0,0 +1,16 @@
+## Functions re-exported from other packages, for ease of use in examples
+
+### From dplyr
+
+#' @importFrom dplyr across
+#' @export
+dplyr::across
+
+#' @importFrom dplyr mutate
+#' @export
+dplyr::mutate
+
+#' @importFrom dplyr where
+#' @export
+dplyr::where
+
